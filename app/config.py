c18a71804f5797e56bcebb7211582e69ec41e2e2
Zhulian Ginev <zhulian.ginev@kiteworks.com>
2025-05-19 09:13:38 +0300

    add initial skeleton

---
+++ app/config.py
@@ -0,0 +1,9 @@
+import os
+
+
+def get_database_url():
+    """
+    Returns the database URL from environment variables.
+    See: https://12factor.net/config
+    """
+    return os.environ.get("DATABASE_URL", "mysql+pymysql://root:password@db:3306/chronos")
